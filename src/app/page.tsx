f7ec2d748d3eeebbaaca4e6d92f1c7e1b1659366
rahucode1 <rahucode1@outlook.com>
2025-07-24 11:04:02 +0530

    removed grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,41 @@
 // src/app/page.tsx
-import { Box, Typography, Container, Button, Grid, Paper } from "@mui/material";
+import { Box, Typography, Container, Paper } from "@mui/material";
 import GoldRateCard from "./_Component/GoldRateCard";
 import ClientFunctions from "./_Component/ClientFunctions";
-
 
 export default function Home() {
   return (
     <Box>
-
-
-      {/* Info Grid Section */}
+      {/* Info Section */}
       <Container sx={{ py: 8 }}>
-        <Grid container spacing={4}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            gap: 4,
+          }}
+        >
           {/* Gold Rate Section */}
-          <Grid item xs={12} md={6}>
+          <Box sx={{ flex: 1 }}>
             <Paper elevation={3} sx={{ p: 4 }}>
               <Typography variant="h5" gutterBottom>
                 Live Gold Rate
               </Typography>
               <GoldRateCard />
             </Paper>
-          </Grid>
+          </Box>
+
           {/* News Section */}
-          <Grid item xs={12} md={6}>
+          <Box sx={{ flex: 1 }}>
             <Paper elevation={3} sx={{ p: 4 }}>
               <Typography variant="h5" gutterBottom>
                 Latest News
-                <ClientFunctions />
               </Typography>
-     
+              <ClientFunctions />
             </Paper>
-          </Grid>
-        </Grid>
+          </Box>
+        </Box>
       </Container>
-
-      {/* CTA Section */}
- 
     </Box>
   );
 }
